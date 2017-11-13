efbb54286839eef656349766f1874bd5867a688b
AlexToksin <griffin-sk@mail.ru>
2017-11-13 18:20:48 +0300

    add file vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Index from '@/components/index'
+import Form from '@/components/form'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Index',
+      component: Index
+    },
+      {
+          path: '/form',
+          name: 'Form',
+          component: Form
+      }
   ]
 })
