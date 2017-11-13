870703fe71410f309b58421a46db3971a4997d9a
AlexToksin <griffin-sk@mail.ru>
2017-11-13 18:28:43 +0300

    add file vue

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/index'
 import Form from '@/components/form'
+import Photo from '@/components/photo'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
           path: '/form',
           name: 'Form',
           component: Form
-      }
+      },
+        {
+          path: '/form/photo',
+          name: 'Photo',
+          component: Photo
+        }
   ]
 })
